5e840a6b0a4cb5813c2db818e9dd342ec9fb20b7
Danil Ryabishin <underz1997@gmail.com>
2024-01-27 09:41:54 +0300

    Initial

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+    public static void main(String[] args) {
+        int ticket = 13676;
+        int priceMile = 20;
+        boolean Correct = true;
+
+        int mile = ticket / priceMile;
+
+        System.out.println("Начислено миль: " + mile);
+
+    }
+}
